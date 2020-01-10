a601e6663d7f7ccbeb7594d04f02fea41e4e930c
harshit <harshit.verma@quovantis.com>
2020-01-11 00:44:00 +0530

    code changes with removal of local db

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { CardList } from './component/card-list/card-list.component';
 import { SeacrhBox } from './component/search-box/searchbox.component';
+import axios from 'axios';
 class App extends React.Component {
   constructor(props) {
     super(props)
@@ -11,18 +12,107 @@ class App extends React.Component {
       searchFeild:''
     }
   }
+
+  pokemonArray =  [
+      {
+        "id": 1,
+        "name": "charizard",
+        "generation": "Generation3"
+      },
+      {
+        "id": 2,
+        "name": "golem",
+        "generation": "Generation1"
+      },
+      {
+        "id": 3,
+        "name": "snorlax",
+        "generation": "Generation1"
+      },
+      {
+        "id": 4,
+        "name": "machop",
+        "generation": "Generation2"
+      },
+      {
+        "id": 5,
+        "name": "geodude",
+        "generation": "Generation1"
+      },
+      {
+        "id": 6,
+        "name": "bulbasaur",
+        "generation": "Generation1"
+      },
+      {
+        "id": 7,
+        "name": "raichu",
+        "generation": "Generation2"
+      },
+      {
+        "id": 8,
+        "name": "sandshrew",
+        "generation": "Genertion1"
+      },
+      {
+        "id": 9,
+        "name": "pikachu",
+        "generation": "Generation1"
+      },
+      {
+        "id": 10,
+        "name": "venusaur",
+        "generation": "Generation3"
+      },
+      {
+        "id": 11,
+        "name": "ivysaur",
+        "generation": "Generation2"
+      },
+      {
+        "id": 12,
+        "name": "charmander",
+        "generation": "Generation1"
+      },
+      {
+        "id": 13,
+        "name": "charmeleon",
+        "generation": "Generation2"
+      },
+      {
+        "id": 14,
+        "name": "squirtle",
+        "generation": "Generation1"
+      },
+      {
+        "id": 15,
+        "name": "wartortle",
+        "generation": "Generation2"
+      },
+      {
+        "id": 16,
+        "name": "blastoise",
+        "generation": "Generation3"
+      },
+      {
+        "id": 17,
+        "name": "rattata",
+        "generation": "Generation1"
+      }
+    ]
   
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(users => this.setState({ pokemon: users }));
+    axios.get('http://localhost:300/pokemon')
+      .then(response => this.setState({ pokemon: response.data}))
+      .catch(() => this.setState({pokemon:this.pokemonArray}));
+      
   }
 
 
 
   render() {
     const { pokemon, searchFeild } = this.state
-    const filteredPokemon = pokemon.filter(pokemon=>pokemon.name.toLowerCase().includes(searchFeild.toLowerCase()))
+    const filteredPokemon = pokemon.filter(pokemon => pokemon.name.toLowerCase().includes(searchFeild.toLowerCase()) || pokemon.generation.toLowerCase().includes(searchFeild.toLowerCase()))
     return (
       <div className="App">
         <div><img className="pokeText" alt='pokemon' src={`https://miro.medium.com/max/1920/1*UNMAo8su_wbOENW8cjcEiw.png`}></img></div>
--- src/component/card/card.component.jsx
+++ src/component/card/card.component.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 import './card.styles.css'
 export const Card = (props) => (
     <div className='card-container'>
-        <img alt="pokemon" src={`https://robohash.org/${props.pokemon.id}?set=set2`}/>
-        <h1>{props.pokemon.name}</h1>
-        <h3>{props.pokemon.email}</h3>
+        {/* <img alt="pokemon" src={`https://robohash.org/${props.pokemon.id}?set=set2`} /> */}
+        <img alt="pokemon" className='image' src={`https://img.pokemondb.net/artwork/${props.pokemon.name}.jpg`} />
+        <h1 className='txt'>{props.pokemon.name}</h1>
+        <p>{props.pokemon.generation}</p>
         </div>
 );
